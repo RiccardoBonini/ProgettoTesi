9ffed384169b5e0d84a51db4f351f119f3ee879e
RiccardoBonini <38753018+RiccardoBonini@users.noreply.github.com>
2020-03-06 11:02:04 +0100

    Add files via upload

--- Main.py
+++ Main.py
@@ -14,20 +14,41 @@ for i in range(len(path_strings)):
     path_stringsM.append(path_strings[i].replace('M', ''))
     #path_strings[i].replace('L', '')
 
-print(path_stringsM)
+#print(path_stringsM)
 path_stringsL = []
 for i in range(len(path_strings)):
     path_stringsL.append(path_stringsM[i].replace('L', ''))
 
-print(path_stringsL)
+#print(path_stringsL)
     #path_strings[i].replace('L', '')
+Xcoordinates = []
+Ycoordinates = []
+coordinates = []
 
-for j in range(len(path_stringsL)):
-    for i in path_stringsL[j]:
-         a = float(i)
-         print(a)
+#for i in range(Xcoordinates):
 
-dwg = svgwrite.Drawing('test.svg', profile='tiny')
-dwg.add(dwg.line((327.2, 154.6), (331.4, 218), stroke=svgwrite.rgb(10, 10, 16, '%')))
-dwg.add(dwg.line((160, 149.2), (268.8,148.8), stroke = svgwrite.rgb(10, 10, 16, '%')))
-dwg.save()
+for i in range(len(path_stringsL)):
+    coordinates.append(path_stringsL[i].split())
+print(coordinates)
+
+for j in range(len(coordinates)):
+    for i in range(0,len(coordinates[j]),2):
+        Xcoordinates[j].append(coordinates[j][i])
+print(Xcoordinates)
+
+for j in range(len(coordinates)):
+    for i in range(1,len(coordinates[j]),2):
+        Ycoordinates[j].append(coordinates[j][i])
+print(Ycoordinates)
+
+# for j in range(len(coordinates)):
+#     for i in range(len(coordinates[j])):
+#          a = float(coordinates[j][i])
+#          print(a)
+
+# dwg = svgwrite.Drawing('test.svg', profile='tiny')
+# dwg.add(dwg.line((Xcoordinates[0], Ycoordinates[0][1]), (coordinates[0][-2], coordinates[0][-1]), stroke=svgwrite.rgb(10, 10, 16, '%')))
+# dwg.add(dwg.line((coordinates[1][0], coordinates[1][1]), (coordinates[1][-2], coordinates[1][-1]), stroke = svgwrite.rgb(10, 10, 16, '%')))
+# dwg.save()
+
+
